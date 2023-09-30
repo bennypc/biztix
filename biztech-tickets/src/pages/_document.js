a92a8a4ff7b1adbebea166d080d3e7810e02f192
bennypc <52959626+bennypc@users.noreply.github.com>
2023-09-30 14:19:17 -0400

    detail

--- biztech-tickets/src/pages/_document.js
+++ biztech-tickets/src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>BizTix</title>
+        <link rel='shortcut icon' href='/biztechlogo.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/biztechlogo.png' />
+
+        <meta property='og:title' content='BizTix' />
+
+        <meta property='og:url' content='https://biztix.vercel.app/' />
+        <meta property='og:type' content='website' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
