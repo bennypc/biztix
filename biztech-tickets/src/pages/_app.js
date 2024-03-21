6af5496aa1fd81d52fce5b13aa665a45d0a16c8b
bennypc <52959626+bennypc@users.noreply.github.com>
2024-03-21 01:11:05 -0400

    import statement

--- biztech-tickets/src/pages/_app.js
+++ biztech-tickets/src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import { UserProvider } from '../contexts/UserContext.js';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
