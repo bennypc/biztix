2bfc5e5cbb1d86c94a53a27e8cf504550d342212
bennypc <52959626+bennypc@users.noreply.github.com>
2024-03-22 17:59:57 -0400

    speed insights

--- biztech-tickets/src/pages/_app.js
+++ biztech-tickets/src/pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.css';
 import { UserProvider } from '../contexts/UserContext.js';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </UserProvider>
   );
 }
